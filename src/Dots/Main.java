e8877f460135a5f5a3d9330b5c52da9c4f6e646b
Luis Arce <luis.arce@jalasoft.com>
2016-05-18 14:44:11 -0400

    Added Dots

---
+++ src/Dots/Main.java
@@ -0,0 +1,23 @@
+package Dots;
+import java.util.Scanner;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      int t = in.nextInt();
+      int nextRow;
+
+      while(--t >= 0) {
+    	  int n = in.nextInt();
+    	  nextRow = 1;
+    	  while (n > 0) {
+    		  n -= nextRow;
+    		  nextRow += 1;
+    	  }
+    	  if (n == 0) System.out.println("Yes");
+    	  else System.out.println("No");
+      }
+   }
+}
